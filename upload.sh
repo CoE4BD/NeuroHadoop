52822240f4d62a576bde6a1b99c8a9eff691777b
Brad Rubin <BradRubin@BradRubin.com>
2012-05-19 12:58:43 -0500

    Ant support for upload

--- upload.sh
+++
@@ -1,3 +0,0 @@
-scp -r ~/Dropbox/workspace/NeuroHadoop/test brad@oss228-01.gps.stthomas.edu:/home/brad/NeuroHadoop2
-scp -r ~/Dropbox/workspace/NeuroHadoop/build/lib brad@oss228-01.gps.stthomas.edu:/home/brad/NeuroHadoop2
-scp -r ~/Dropbox/workspace/NeuroHadoop/script brad@oss228-01.gps.stthomas.edu:/home/brad/NeuroHadoop2
