288c62353cb3162c57b095949a00128af74b1c57
Brad Rubin <BradRubin@BradRubin.com>
2013-08-21 18:33:35 -0500

    Fixes for new cluster and CDH4.3

--- script/shell/upload.sh
+++ script/shell/upload.sh
@@ -1,3 +1,3 @@
-scp -r ~/Dropbox/workspace/NeuroHadoop/build/lib brad@hc1.gps.stthomas.edu:/home/brad/NeuroHadoop
-scp -r ~/Dropbox/workspace/NeuroHadoop/script brad@hc1.gps.stthomas.edu:/home/brad/NeuroHadoop
-scp -r ~/Dropbox/workspace/NeuroHadoop/test brad@hc1.gps.stthomas.edu:/home/brad/NeuroHadoop
+scp -r ~/Dropbox/workspace/NeuroHadoop/build/lib brad@hc.gps.stthomas.edu:/home/brad/NeuroHadoop
+scp -r ~/Dropbox/workspace/NeuroHadoop/script brad@hc.gps.stthomas.edu:/home/brad/NeuroHadoop
+scp -r ~/Dropbox/workspace/NeuroHadoop/test brad@hc.gps.stthomas.edu:/home/brad/NeuroHadoop
