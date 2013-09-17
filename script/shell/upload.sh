8eef518e04dfce9b6c853f446b9e73ce4ffe8161
Brad Rubin <BradRubin@BradRubin.com>
2013-09-17 11:49:23 -0500

    Change URL for new cluster.

--- script/shell/upload.sh
+++ script/shell/upload.sh
@@ -1,3 +1,3 @@
-scp -r ~/Dropbox/workspace/NeuroHadoop/build/lib brad@hc.gps.stthomas.edu:/home/brad/NeuroHadoop
-scp -r ~/Dropbox/workspace/NeuroHadoop/script brad@hc.gps.stthomas.edu:/home/brad/NeuroHadoop
-scp -r ~/Dropbox/workspace/NeuroHadoop/test brad@hc.gps.stthomas.edu:/home/brad/NeuroHadoop
+scp -r ~/Dropbox/workspace/NeuroHadoop1/build/lib brad@hc.gps.stthomas.edu:/home/brad/NeuroHadoop
+scp -r ~/Dropbox/workspace/NeuroHadoop1/script brad@hc.gps.stthomas.edu:/home/brad/NeuroHadoop
+scp -r ~/Dropbox/workspace/NeuroHadoop1/test brad@hc.gps.stthomas.edu:/home/brad/NeuroHadoop
